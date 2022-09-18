e7193a079d95f203a592e01a1b35914de75d9891
Matthew Drumm <drumm.mv@gmail.com>
2022-09-18 18:57:56 -0400

    update for challenge part 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -12,3 +12,14 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #Summary
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,mechaCar_mpg_data)) #generate summary statistics
+
+# Read in the CSV file for Suspension_Coil.csv
+Suspension_Coil_data <- read.csv(file="Resources/Suspension_Coil.csv")
+
+# get the "Total" mean, median, variance, and standard deviation of the suspension coil’s PSI column
+total_summary <- summarize(Suspension_Coil_data, Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
+
+# get the individual "Lot" mean, median, variance, and standard deviation of the suspension coil’s PSI column
+lot_summary <- Suspension_Coil_data %>%
+  group_by(Manufacturing_Lot) %>%
+  summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
